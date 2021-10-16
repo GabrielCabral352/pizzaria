a2cdf717a6e072a61b2c54d9d8561c9d88682526
GabrielCabral352 <gabrielcabral352@gmail.com>
2021-10-16 11:37:24 -0300

    resp card and i pop

--- cardapio/views.py
+++ cardapio/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def index(request):
 
-	return render(request, 'cardapio/cardapio.html')
+	return render(request, 'cardapio/cardapio.html', {'dados': [1,2,3,4,5,6,7,8,9,0]})
---
+++ static/js/cardapio.js
@@ -0,0 +1,18 @@
+document.addEventListener('click', function({ target }) {
+    if (target.id == "popupBg"){
+        document.querySelector(".popup").style.display = "none"
+        document.querySelector(".popupBg").style.display = "none"
+    }
+}, true);
+
+document.getElementById("closeBTN").addEventListener('click', function({ target }) {
+    document.querySelector(".popup").style.display = "none"
+    document.querySelector(".popupBg").style.display = "none"
+
+}, true);
+function popup(tag){
+    document.querySelector(".popup").style.display = "block";
+    document.querySelector(".popupBg").style.display = "block";
+
+}
+
