1b376926d44a4df6f6e3e230bea0a49af97af791
GabrielCabral352 <gabrielcabral352@gmail.com>
2021-10-19 07:54:29 -0300

    models

--- cardapio/models.py
+++ cardapio/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
-from django.db import models
-
 
 
 class Cardapio(models.Model):
     descricao = models.CharField(max_length=50)
-    valor = models.DecimalField(max_digits=8)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
 
 
 
@@ -16,21 +14,21 @@ class Cliente(models.Model):
 
 class Produto(models.Model):
     nome = models.CharField(max_length=20)
-    estoque = models.IntegerField(max_digits=10)
+    estoque = models.IntegerField()
 
 
 class Items(models.Model):
-    quantidade = models.IntegerField(max_digits=10)
-    id_card = models.ForeignKey(Cardapio, on_delete=models.DO_NOTTING)
-    id_prod = models.ForeignKey(Produto, on_delete=models.DO_NOTTING)
+    quantidade = models.IntegerField()
+    id_card = models.ForeignKey(Cardapio, on_delete=models.CASCADE)
+    id_prod = models.ForeignKey(Produto, on_delete=models.CASCADE)
 
 
 class Sub_pedido(models.Model):
-    id_card = models.ForeignKey(Cardapio, on_delete=models.DO_NOTTING)
-    quantidade = models.IntegerField(max_digits=10)
+    id_card = models.ForeignKey(Cardapio, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
 
 
 class Pedido(models.Model):
-    id_cliente = models.ForeignKey(Cliente, on_delete=models.DO_NOTTING)
-    id_subp = models.ForeignKey(Sub_pedido)
+    id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    id_subp = models.ForeignKey(Sub_pedido, on_delete=models.CASCADE)
 
