45ebfcf59ea80bd45e96bd236f9669ad568dfeed
lfds <l.fds@yahoo.com>
2021-10-19 22:12:21 -0300

    models cardapio finalizado

--- cardapio/admin.py
+++ cardapio/admin.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
-from cardapio.models import Cardapio, Items, Produto
-from home.models import Cliente, Sub_pedido, Pedido
+from cardapio.models import Cardapio, Item, Produto
 
 
 class itemsAdmin(admin.ModelAdmin):
-    list_display = ('id_card','id_prod', 'quantidade')
-    list_filter = ('id_card', )
-
-# class produtoAdmin(admin.ModelAdmin):
-#     list_display = ('')
+    list_display = ('id_card', 'id_prod', 'quantidade')
+    list_filter = ('id_card',)
 
 
-admin.site.register(Cardapio)
-admin.site.register(Items, itemsAdmin)
-admin.site.register(Produto)
+class cardAdmin(admin.ModelAdmin):
+    list_display = ('descricao', 'valor')
+    list_filter = ('tipo', )
+
+
+class prodAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'estoque')
+
+
+admin.site.register(Cardapio, cardAdmin)
+admin.site.register(Item, itemsAdmin)
+admin.site.register(Produto, prodAdmin)
--- cardapio/models.py
+++ cardapio/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 
 class Cardapio(models.Model):
-    descricao = models.CharField(max_length=50)
+    bebida = 'bebida'
+    comida = 'comida'
+
+    CHOICES = (
+        (bebida, 'bebida'),
+        (comida, 'comida'),
+    )
+
+    descricao = models.CharField(max_length=50, verbose_name='Nome')
     valor = models.DecimalField(max_digits=10, decimal_places=2)
+    tipo = models.CharField(max_length=255, choices=CHOICES)
 
     class Meta:
         db_table = 'cardapio'
@@ -23,14 +32,13 @@ class Produto(models.Model):
         return self.nome
 
 
-class Items(models.Model):
+class Item(models.Model):
+    id_card = models.ForeignKey(Cardapio, on_delete=models.CASCADE, verbose_name='Receita')
+    id_prod = models.ForeignKey(Produto, on_delete=models.CASCADE, verbose_name='Ingrediente')
     quantidade = models.IntegerField()
-    id_card = models.ForeignKey(Cardapio, on_delete=models.CASCADE, verbose_name='Sabor')
-    id_prod = models.ForeignKey(Produto, on_delete=models.CASCADE, verbose_name='Ingredientes')
 
     class Meta:
         db_table = 'Items'
 
     def __str__(self):
         return str(self.id)
-
